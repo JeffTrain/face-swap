ddf5f2d2f980fd3d3a686e78ba60f539a96acb4f
Jeff Tian <jeff.tian@outlook.com>
2022-11-09 15:39:49 +0800

    cicd: add github actions

---
+++ .travis/ci.sh
@@ -0,0 +1,10 @@
+docker build -t jefftian/face-swap .
+docker images
+docker run --network host -e CI=true -d -p 127.0.0.1:5000:5000 --name face-swap jefftian/face-swap
+docker ps | grep -q face-swap
+docker ps -aqf "name=face-swap$"
+docker push jefftian/face-swap
+docker logs $(docker ps -aqf name=face-swap$)
+curl localhost:5000 || docker logs $(docker ps -aqf name=face-swap$)
+docker kill face-swap || echo "face-swap killed"
+docker rm face-swap || echo "face-swap removed"
