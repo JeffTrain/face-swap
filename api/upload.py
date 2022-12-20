160e740c1182c3653a4e7f09b63ead45b3bfe431
Jeff Tian <jeff.tian@outlook.com>
2022-12-20 17:18:57 +0800

    fix: base64 encode binary data UNI-86

--- api/upload.py
+++ api/upload.py
@@ -1,7 +1,19 @@
+import base64
+from pprint import pprint
+
 from ariadne import convert_kwargs_to_snake_case
 
 
 @convert_kwargs_to_snake_case
 def resolve_upload_image(obj, info, image_1, image_2):
-    data = image_1.file.read()
-    return {"filename": image_1.filename, "data": data}
+    filename = image_1.filename
+    mimetype = image_1.mimetype
+    data = image_1.read()
+
+    data_base64 = base64.b64encode(data).decode('utf-8')
+
+    return {
+        'filename': filename,
+        'mimetype': mimetype,
+        'data': data_base64,
+    }
