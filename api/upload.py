3b6bd0cf496deaca55290f143e1c8763772ab4a6
Jeff Tian <jeff.tian@outlook.com>
2022-12-20 15:50:37 +0800

    feat: pass image_1 back UNI-86

--- api/upload.py
+++ api/upload.py
@@ -3,5 +3,5 @@ from ariadne import convert_kwargs_to_snake_case
 
 @convert_kwargs_to_snake_case
 def resolve_upload_image(obj, info, image_1, image_2):
-    return 'success'
-
+    data = image_1.file.read()
+    return {"filename": image_1.filename, "data": data}
