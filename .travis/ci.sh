47421fe531246729e1250a2a8422bceea4c2bb53
Jeff Tian <jeff.tian@outlook.com>
2023-01-07 20:38:18 +0800

    cicd: UNI-89, set tag to docker buildgs

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -1,9 +1,9 @@
-docker build -t jefftian/face-swap .
+docker build -t jefftian/face-swap:"$1" .
 docker images
-docker run --network host -e CI=true -d -p 127.0.0.1:5000:5000 --name face-swap jefftian/face-swap
+docker run --network host -e CI=true -d -p 127.0.0.1:5000:5000 --name face-swap jefftian/face-swap:"$1"
 docker ps | grep -q face-swap
 docker ps -aqf "name=face-swap$"
-docker push jefftian/face-swap
+docker push jefftian/face-swap:"$1"
 docker logs $(docker ps -aqf name=face-swap$)
 curl localhost:5000 || docker logs $(docker ps -aqf name=face-swap$)
 docker kill face-swap || echo "face-swap killed"
